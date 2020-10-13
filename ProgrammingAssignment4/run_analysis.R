31a3f7584b6ac4e1cccbc711d80882e99a10d775
Berk Deniz Orhan <berkdenizorhan@gmail.com>
2020-10-13 22:38:22 +0200

    Added comments and fixed variable names
    Some comments are added and variable names fixed to increase readability

--- ProgrammingAssignment4/run_analysis.R
+++ ProgrammingAssignment4/run_analysis.R
@@ -2,7 +2,7 @@ library(dplyr)
 
 filename <- "Coursera_DS3_Final.zip"
 
-# Checking if archieve already exists.
+# Checking if archieve exists.
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileURL, filename, method="curl")
@@ -13,6 +13,7 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(filename) 
 }
 
+# Creating dataframes that store the data of each .txt file
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -22,15 +23,18 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+# Merging training and test sets
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
-Merged_Data <- cbind(Subject, Y, X)
+merged_Data <- cbind(Subject, Y, X)
 
-TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+# Collecting mean and standart deviation
+TidyData <- merged_Data %>% select(subject, code, contains("mean"), contains("std"))
 
 TidyData$code <- activities[TidyData$code, 2]
 
+# Name the datasets
 names(TidyData)[2] = "activity"
 names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
 names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
@@ -45,9 +49,11 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
+# Generate a dataframe with final data
 FinalData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
+
+# Write the final data to a .csv file
 write.csv(FinalData, "FinalData.csv", row.names = FALSE)
 
-class(FinalData)
